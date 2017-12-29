8deedbe1d3abfce0a635dfcf6e93f5b618cfda50
lxfly2000 <gaoboyuan8@qq.com>
2017-12-29 20:04:58 +0800

    Init.

---
+++ hijackmidi/hijackmidi.cpp
@@ -0,0 +1,122 @@
+#ifndef WIN32
+#error 该程序目前只能在x86中编译。
+#endif
+
+#include<vector>
+#include<Windows.h>
+#include<CommCtrl.h>
+#pragma comment(lib,"winmm.lib")
+#pragma comment(lib,"ComCtl32.lib")
+#pragma comment(linker,"\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
+processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
+UINT ChooseMidiOutDevice(UINT defaultID)
+{
+	UINT selID = defaultID;
+	MIDIOUTCAPS moc;
+	TCHAR msg[64];
+	TASKDIALOGCONFIG tc = { 0 };
+	std::vector<TASKDIALOG_BUTTON> tb;
+	tc.cbSize = sizeof tc;
+	tc.dwCommonButtons = TDCBF_CANCEL_BUTTON;
+	tc.hInstance = NULL;
+	tc.pszContent = msg;
+	tc.pszMainInstruction = TEXT("选择MIDI输出设备");
+	tc.pszMainIcon = TD_INFORMATION_ICON;
+	tc.dwFlags = TDF_USE_COMMAND_LINKS;
+
+	UINT midiOutCount = midiOutGetNumDevs();
+	midiOutGetDevCaps(defaultID, &moc, sizeof moc);
+	wsprintf(msg, TEXT("程序指定的设备为：\n[%d]%s"), defaultID, moc.szPname);
+	std::vector<std::wstring> descptr(midiOutCount + 1);
+	for (int i = -1; i < (int)midiOutCount; i++)
+	{
+		midiOutGetDevCaps(i, &moc, sizeof moc);
+		TCHAR devdesc[60];
+		wsprintf(devdesc, TEXT("[%d]%s"), i, moc.szPname);
+		if ((UINT)i == MIDI_MAPPER)
+			lstrcat(devdesc, TEXT("\nMIDI 映射器"));
+		if ((UINT)i == defaultID)
+		{
+			if (defaultID == MIDI_MAPPER)
+				lstrcat(devdesc, TEXT("，"));
+			else
+				lstrcat(devdesc, TEXT("\n"));
+			lstrcat(devdesc, TEXT("程序指定的设备"));
+		}
+		descptr[i + 1] = devdesc;
+		tb.push_back({ 1000 + i,descptr[i + 1].c_str() });
+	}
+	tc.pButtons = tb.data();
+	tc.cButtons = tb.size();
+
+	TaskDialogIndirect(&tc, reinterpret_cast<int*>(&selID), NULL, NULL);
+	if (selID >= 999)
+		return selID - 1000;
+	return defaultID;
+}
+
+void StartHijack();
+void StopHijack();
+
+MMRESULT WINAPI Redirect_midiOutOpen(LPHMIDIOUT phmo, UINT uDeviceID, DWORD_PTR dwCallback,
+	DWORD_PTR dwInstance, DWORD fdwOpen)
+{
+	uDeviceID = ChooseMidiOutDevice(uDeviceID);
+	StopHijack();
+	uDeviceID = midiOutOpen(phmo, uDeviceID, dwCallback, dwInstance, fdwOpen);
+	StartHijack();
+	return uDeviceID;
+}
+
+using FUNCTYPE = decltype(midiOutOpen);
+FUNCTYPE *funcOriginal = nullptr;
+DWORD protectOriginal;
+char apientry[5];
+DWORD hTargetPid;
+HANDLE hTargetProcess;
+
+void StartHijack()
+{
+	hTargetProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, hTargetPid);
+	funcOriginal = (FUNCTYPE*)GetProcAddress(LoadLibrary(TEXT("winmm.dll")), "midiOutOpen");
+	memcpy(apientry, funcOriginal, 5);
+	DWORD entryaddr = reinterpret_cast<DWORD>(Redirect_midiOutOpen) - reinterpret_cast<DWORD>(funcOriginal) - 5;
+	char asmcode[5] = { '\xe9' };//JMP
+	memcpy(asmcode + 1, &entryaddr, 4);
+	VirtualProtectEx(hTargetProcess, funcOriginal, 5, PAGE_EXECUTE_READWRITE, &protectOriginal);
+	WriteProcessMemory(hTargetProcess, funcOriginal, asmcode, 5, NULL);
+	VirtualProtectEx(hTargetProcess, funcOriginal, 5, protectOriginal, &protectOriginal);
+}
+
+void StopHijack()
+{
+	VirtualProtectEx(hTargetProcess, funcOriginal, 5, PAGE_EXECUTE_READWRITE, &protectOriginal);
+	WriteProcessMemory(hTargetProcess, funcOriginal, apientry, 5, NULL);
+	VirtualProtectEx(hTargetProcess, funcOriginal, 5, protectOriginal, &protectOriginal);
+	CloseHandle(hTargetProcess);
+}
+
+BOOL WINAPI DllMain(HINSTANCE hInstDll, DWORD fdwReason, LPVOID lpvReserved)
+{
+	switch (fdwReason)
+	{
+	case DLL_PROCESS_ATTACH:
+		DisableThreadLibraryCalls(hInstDll);
+		hTargetPid = GetCurrentProcessId();
+		StartHijack();
+		break;
+	case DLL_PROCESS_DETACH:
+		StopHijack();
+		break;
+	case DLL_THREAD_ATTACH:break;
+	case DLL_THREAD_DETACH:break;
+	}
+	return TRUE;
+}
+
+#pragma comment(linker,"/Export:MidiHook=_MidiHook@12")
+extern "C" LRESULT WINAPI MidiHook(int code, WPARAM w, LPARAM l)
+{
+	return CallNextHookEx(NULL, code, w, l);
+}
---
+++ hookmidi/hookmidi.cpp
@@ -0,0 +1,106 @@
+//http://www.freebuf.com/articles/system/93413.html
+//http://www.freebuf.com/articles/system/94693.html
+#include<vector>
+#include<Windows.h>
+#include<TlHelp32.h>
+
+DWORD GetFirstThreadID(DWORD pid)
+{
+	HANDLE h = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0);
+	if (h == INVALID_HANDLE_VALUE)
+		return 0;
+	THREADENTRY32 te;
+	te.dwSize = sizeof(te);
+	for (BOOL tfound = Thread32First(h, &te); tfound; tfound = Thread32Next(h, &te))
+	{
+		if (te.dwSize >= FIELD_OFFSET(THREADENTRY32, th32OwnerProcessID) + sizeof(te.th32OwnerProcessID))
+		{
+			if (te.th32OwnerProcessID == pid)
+			{
+				HANDLE hThread = OpenThread(READ_CONTROL, FALSE, te.th32ThreadID);
+				if (hThread)
+					return te.th32ThreadID;
+			}
+		}
+	}
+	CloseHandle(h);
+	return 0;
+}
+
+bool ChooseExecuteFile(std::wstring &path)
+{
+	TCHAR cpath[MAX_PATH] = TEXT(""), cname[MAX_PATH] = TEXT("");
+	OPENFILENAME ofn = { 0 };
+	ofn.lStructSize = sizeof OPENFILENAME;
+	ofn.hwndOwner = nullptr;
+	ofn.hInstance = nullptr;
+	ofn.lpstrFilter = TEXT("应用程序\0*.exe;*.com\0所有文件\0*\0\0");
+	ofn.lpstrFile = cpath;
+	ofn.nMaxFile = MAX_PATH;
+	ofn.lpstrFileTitle = cname;
+	ofn.nMaxFileTitle = MAX_PATH;
+	ofn.Flags = OFN_HIDEREADONLY;
+	if (GetOpenFileName(&ofn))
+	{
+		path = cpath;
+		return true;
+	}
+	return false;
+}
+
+int WINAPI wWinMain(HINSTANCE hI, HINSTANCE hPvI, LPWSTR param, int nShow)
+{
+	SHELLEXECUTEINFO se = { 0 };
+	se.cbSize = sizeof se;
+	se.hwnd = NULL;
+	se.lpVerb = TEXT("open");
+	se.lpFile = NULL;
+	se.lpParameters = NULL;
+	se.fMask = SEE_MASK_NOCLOSEPROCESS;
+	se.nShow = SW_SHOW;
+	TCHAR szcd[MAX_PATH];
+	GetCurrentDirectory(ARRAYSIZE(szcd), szcd);
+	se.lpDirectory = szcd;
+
+	std::wstring separam, sefullpath;
+	switch (__argc)
+	{
+	default:
+	case 3:
+		for (int i = 2; i < __argc; i++)
+		{
+			if (i > 2)
+				separam += TEXT(" ");
+			separam += __wargv[i];
+		}
+		se.lpParameters = separam.c_str();
+	case 2:
+	{
+		TCHAR szfp[MAX_PATH];
+		GetFullPathName(__wargv[1], ARRAYSIZE(szfp), szfp, NULL);
+		sefullpath = szfp;
+		se.lpFile = sefullpath.c_str();
+	}
+		break;
+	case 1:
+		if (!ChooseExecuteFile(separam))
+			return 0;
+		GetCurrentDirectory(ARRAYSIZE(szcd), szcd);
+		se.lpFile = separam.c_str();
+		break;
+	case 0:
+		return E_INVALIDARG;
+		break;
+	}
+	ShellExecuteEx(&se);
+	DWORD hTargetPid = GetProcessId(se.hProcess);
+	HMODULE hdll = LoadLibrary(TEXT("hijackmidi"));
+	if (GetProcAddress(hdll, "MidiHook") == NULL)
+		MessageBox(NULL, TEXT("没有找到 MidiHook 函数。"), NULL, MB_ICONERROR);
+	HHOOK hhkMidi = SetWindowsHookEx(WH_DEBUG, (HOOKPROC)GetProcAddress(hdll, "MidiHook"),
+		hdll, GetFirstThreadID(hTargetPid));
+	WaitForSingleObject(se.hProcess, INFINITE);
+	UnhookWindowsHookEx(hhkMidi);
+
+	return 0;
+}
