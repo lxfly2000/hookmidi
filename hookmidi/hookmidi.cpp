06c80180f1b2f037a1aac03c36ab3cebd40031ea
lxfly2000 <gaoboyuan8@qq.com>
2018-03-22 16:08:10 +0800

    Update.

--- hookmidi/hookmidi.cpp
+++ hookmidi/hookmidi.cpp
@@ -4,20 +4,56 @@
 #define PROFILE_NAME ".\\hook.ini"
 #define KEY_HOOK_FILE "file"
 #define KEY_HOOK_FUNC "function"
+#define KEY_HOOK_TYPE "type"
+#include<Windows.h>
 #ifdef HOOKMIDI
 #define DEFAULT_HOOK_FILE "hijackmidi.dll"
 #define DEFAULT_HOOK_FUNC "MidiHook"
+#define DEFAULT_HOOK_TYPE WH_SHELL
 #else
 #define DEFAULT_HOOK_FILE "hook.dll"
 #define DEFAULT_HOOK_FUNC "HookCallback"
+#define DEFAULT_HOOK_TYPE WH_MSGFILTER
 #endif
 //http://www.freebuf.com/articles/system/93413.html
 //http://www.freebuf.com/articles/system/94693.html
-#include<Windows.h>
 #include<windowsx.h>
 #include"resource.h"
 #pragma comment(linker,"\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
 processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
+typedef struct tagHookTypeItem
+{
+	int typeId;
+	LPCTSTR descriptionText;
+}HookTypeItem;
+
+const HookTypeItem hookTypes[] = {
+	{WH_MSGFILTER,TEXT("[-1]消息过滤器")},
+	{WH_JOURNALRECORD,TEXT("[0]宏记录")},
+	{WH_JOURNALPLAYBACK,TEXT("[1]宏回放")},
+	{WH_KEYBOARD,TEXT("[2]键盘")},
+	{WH_GETMESSAGE,TEXT("[3]消息队列")},
+	{WH_CALLWNDPROC,TEXT("[4]窗口过程")},
+	{WH_CBT,TEXT("[5]系统基本消息")},
+	{WH_SYSMSGFILTER,TEXT("[6]系统消息过滤器")},
+	{WH_MOUSE,TEXT("[7]鼠标")},
+	{WH_DEBUG,TEXT("[9]调试")},
+	{WH_SHELL,TEXT("[10]外壳应用程序")},
+	{WH_FOREGROUNDIDLE,TEXT("[11]程序前台线程空闲")},
+	{WH_CALLWNDPROCRET,TEXT("[12]窗口过程返回")},
+	{WH_KEYBOARD_LL,TEXT("[13]键盘底层")},
+	{WH_MOUSE_LL,TEXT("[14]鼠标底层")}
+};
+
+int GetIndexOfHookType(int type)
+{
+	int index = 0;
+	for (int i = 0; i < ARRAYSIZE(hookTypes); i++)
+		if (hookTypes[i].typeId == type)
+			index = i;
+	return index;
+}
 
 BOOL ChooseHookFile(HWND hParent, LPTSTR path, int lpath)
 {
@@ -55,7 +91,7 @@ void StopHook()
 		return;
 	if (!UnhookWindowsHookEx(hHook))
 	{
-		TCHAR msg[40];
+		TCHAR msg[21];
 		DWORD e = GetLastError();
 		if (e)
 		{
@@ -88,6 +124,11 @@ void StartHook()
 	TCHAR dllfile[MAX_PATH];
 	char funcname[MAX_FUNCNAME];
 	GetDlgItemText(hDlg, IDC_EDIT_HOOKFILE, dllfile, ARRAYSIZE(dllfile));
+	if (lstrcmpi(wcsrchr(dllfile, '.'), TEXT(".dll")) != 0)
+	{
+		if (MessageBox(hDlg, TEXT("你选择的文件并不是 DLL 文件，确定继续？"), GetFileName(dllfile), MB_ICONWARNING | MB_OKCANCEL) != IDOK)
+			return;
+	}
 	GetDlgItemTextA(hDlg, IDC_EDIT_HOOKFUNC, funcname, ARRAYSIZE(funcname));
 	if (!hDll)
 	{
@@ -109,22 +150,26 @@ void StartHook()
 	//个人推测WH_DEBUG开销最大而WH_SHELL开销最小
 	//实际上这样改过后就不需要监视目标进程了，因为钩子是全局的
 	//顺序上只要保证钩子在目标程序前启动就行了
-	hHook = SetWindowsHookEx(WH_SHELL, fHookCallback, hDll, 0);
+	int chosenType = hookTypes[ComboBox_GetCurSel(GetDlgItem(hDlg, IDC_COMBO_HOOKTYPE))].typeId;
+	hHook = SetWindowsHookEx(chosenType, fHookCallback, hDll, 0);
 	if (hHook == NULL)
 	{
 		StopHook();
-		TCHAR msg[40];
-		wsprintf(msg, TEXT("无法设置Hook：%#x\n请尝试重新启动。"), GetLastError());
+		TCHAR msg[53];
+		wsprintf(msg, TEXT("无法设置Hook：%#x\n请尝试重新启动该程序；如果还是出错则可能是该类型的钩子不受支持。"), GetLastError());
 		MessageBox(hDlg, msg, NULL, MB_ICONERROR);
 		return;
 	}
-	TCHAR title[300];
-	wsprintf(title, TEXT("Hook 已启动：%s"), GetFileName(dllfile));
+	TCHAR title[533];
+	wsprintf(title, TEXT("Hook 已启动：%s, %S [%d]"), GetFileName(dllfile), funcname, chosenType);
 	SetWindowText(hDlg, title);
 	SetDlgItemText(hDlg, IDOK, TEXT("停止"));
 #ifndef HOOKMIDI
 	WritePrivateProfileStringA(APP_IDENTIFIER, KEY_HOOK_FUNC, funcname, PROFILE_NAME);
 	WritePrivateProfileString(TEXT(APP_IDENTIFIER), TEXT(KEY_HOOK_FILE), dllfile, TEXT(PROFILE_NAME));
+	TCHAR *const typeStr = title;
+	wsprintf(typeStr, TEXT("%d"), chosenType);
+	WritePrivateProfileString(TEXT(APP_IDENTIFIER), TEXT(KEY_HOOK_TYPE), typeStr, TEXT(PROFILE_NAME));
 #endif
 }
 
@@ -146,34 +191,49 @@ BOOL SetHookFunction(LPCTSTR funcname)
 	return SetDlgItemText(hDlg, IDC_EDIT_HOOKFUNC, funcname);
 }
 
+int SetHookType(int type)
+{
+	return ComboBox_SetCurSel(GetDlgItem(hDlg, IDC_COMBO_HOOKTYPE), GetIndexOfHookType(type));
+}
+
+void DialogInit(HWND h)
+{
+	hDlg = h;
+	Edit_LimitText(GetDlgItem(h, IDC_EDIT_HOOKFILE), MAX_PATH - 1);
+	Edit_LimitText(GetDlgItem(h, IDC_EDIT_HOOKFUNC), MAX_FUNCNAME - 1);
+	for (auto &e : hookTypes)
+		ComboBox_AddString(GetDlgItem(h, IDC_COMBO_HOOKTYPE), e.descriptionText);
+
+	TCHAR buf[MAX_PATH];
+	GetPrivateProfileString(TEXT(APP_IDENTIFIER), TEXT(KEY_HOOK_FILE), TEXT(DEFAULT_HOOK_FILE),
+		buf, ARRAYSIZE(buf), TEXT(PROFILE_NAME));
+	SetHookFile(buf);
+	GetPrivateProfileString(TEXT(APP_IDENTIFIER), TEXT(KEY_HOOK_FUNC), TEXT(DEFAULT_HOOK_FUNC),
+		buf, ARRAYSIZE(buf), TEXT(PROFILE_NAME));
+	SetHookFunction(buf);
+	SetHookType(GetPrivateProfileInt(TEXT(APP_IDENTIFIER), TEXT(KEY_HOOK_TYPE), DEFAULT_HOOK_TYPE, TEXT(PROFILE_NAME)));
+
+	StopHook();
+	switch (__argc)
+	{
+	case 4:
+		SetHookType(_wtoi(__wargv[3]));
+	case 3:
+		SetHookFunction(__wargv[2]);
+	case 2:
+		SetHookFile(__wargv[1]);
+		break;
+	}
+	if (__argc > 2)
+		StartHook();
+}
+
 INT_PTR WINAPI DialogCallback(HWND h, UINT m, WPARAM w, LPARAM l)
 {
 	switch (m)
 	{
 	case WM_INITDIALOG:
-		hDlg = h;
-		Edit_LimitText(GetDlgItem(h, IDC_EDIT_HOOKFILE), MAX_PATH-1);
-		Edit_LimitText(GetDlgItem(h, IDC_EDIT_HOOKFUNC), MAX_FUNCNAME-1);
-		{
-			TCHAR buf[MAX_PATH];
-			GetPrivateProfileString(TEXT(APP_IDENTIFIER), TEXT(KEY_HOOK_FILE), TEXT(DEFAULT_HOOK_FILE),
-				buf, ARRAYSIZE(buf), TEXT(PROFILE_NAME));
-			SetHookFile(buf);
-			GetPrivateProfileString(TEXT(APP_IDENTIFIER), TEXT(KEY_HOOK_FUNC), TEXT(DEFAULT_HOOK_FUNC),
-				buf, ARRAYSIZE(buf), TEXT(PROFILE_NAME));
-			SetHookFunction(buf);
-		}
-		StopHook();
-		switch (__argc)
-		{
-		case 3:
-			SetHookFunction(__wargv[2]);
-		case 2:
-			SetHookFile(__wargv[1]);
-			break;
-		}
-		if (__argc > 2)
-			StartHook();
+		DialogInit(h);
 		break;
 	case WM_COMMAND:
 		switch (LOWORD(w))
@@ -194,6 +254,14 @@ INT_PTR WINAPI DialogCallback(HWND h, UINT m, WPARAM w, LPARAM l)
 		}
 			break;
 		}
+		break;
+	case WM_DROPFILES:
+	{
+		TCHAR path[MAX_PATH];
+		DragQueryFile((HDROP)w, 0, path, MAX_PATH);
+		DragFinish((HDROP)w);
+		SetHookFile(path);
+	}
 		break;
 	}
 	return 0;
--- hookmidi/resource.h
+++ hookmidi/resource.h
@@ -6,6 +6,7 @@
 #define IDC_EDIT_HOOKFILE               1001
 #define IDC_BUTTON_BROWSE               1002
 #define IDC_EDIT_HOOKFUNC               1003
+#define IDC_COMBO_HOOKTYPE              1004
 
 // Next default values for new objects
 // 
@@ -13,7 +14,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        103
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_CONTROL_VALUE         1005
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
